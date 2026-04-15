44abb8dbe88d617165fc00e58e8091d085c88768
Nikolas3333 <1.123a@inbox.ru>
2026-04-15 23:28:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -9627,9 +9627,9 @@ function createHangarRoomEnvironment(){
         dockGroup.add(pad);
 
         const plaque = createHangarPlaqueBoard(4.6, 1.86);
-        plaque.position.set(x < 0 ? 2.2 : -2.2, 1.3, -0.6);
-        plaque.rotation.x = -0.06;
-        plaque.rotation.y = x < 0 ? -Math.PI/2 : Math.PI/2;
+        plaque.position.set(x < 0 ? 1.35 : -1.35, 1.26, 0.0);
+        plaque.rotation.x = -0.12;
+        plaque.rotation.y = x < 0 ? -Math.PI / 2 : Math.PI / 2;
         plaque.rotation.z = 0;
         dockGroup.add(plaque);
 
